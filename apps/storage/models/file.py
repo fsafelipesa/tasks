16d35158d3967d7494d9729192d03c7956d7afa0
Felipe Sá <78086873+fsafelipesa@users.noreply.github.com>
2021-02-10 20:20:30 -0300

    Add files via upload

--- apps/storage/models/file.py
+++ apps/storage/models/file.py
@@ -6,7 +6,7 @@ from apps.storage.models import Directory
 class File(models.Model):
     parent = models.ForeignKey(Directory, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    contents = models.BinaryField()
+    contents = models.BinaryField(editable=True)
 
     def path(self):
         return f'{self.parent.path()}/{self.name}'
